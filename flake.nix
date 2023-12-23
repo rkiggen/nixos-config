3a9045ed7702f9d32713816eb7d482c696ee6414
Kiggen, Rob <rob.kiggen+github@gmail.com>
2023-12-23 19:24:52 +0100

    Updated host configuration and profile for l231028a

--- flake.nix
+++ flake.nix
@@ -4,15 +4,15 @@
   inputs = { 							
 	
     # Nix library functions
-    nixpkgs-for-lib.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-for-lib.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     # NixOS
     nixpkgs-for-nixos-l211008a.url = "github:nixos/nixpkgs/nixos-23.05";
-    nixpkgs-for-nixos-l231028a.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs-for-nixos-l231028a.url = "github:nixos/nixpkgs/nixos-23.11";
 
     # Stable, general purpose
     nixpkgs-from-stable-l211008a.url = "github:NixOS/nixpkgs/nixos-23.05";
-    nixpkgs-from-stable-l231028a.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs-from-stable-l231028a.url = "github:NixOS/nixpkgs/nixos-23.11";
 
     # Unstable, general purpose
     nixpkgs-from-unstable-l211008a.url = "github:nixos/nixpkgs/nixos-unstable";
@@ -21,11 +21,11 @@
     # Rider 
     nixpkgs-for-rider-l211008a.url = "github:nixos/nixpkgs/nixos-23.05"; # Rider 2023.1.2
     #nixpkgs-for-rider-l211008a.url = "https://github.com/NixOS/nixpkgs/archive/64c27498901f104a11df646278c4e5c9f4d642db.tar.gz"; # Rider 22.3.2 (unstable channel)
-    nixpkgs-for-rider-l231028a.url = "github:nixos/nixpkgs/nixos-23.05"; # Rider 2023.1.2
+    nixpkgs-for-rider-l231028a.url = "github:nixos/nixpkgs/nixos-23.11"; # Rider 2023.1.2
 
     # Home-manager
     home-manager-l211008a.url = "github:nix-community/home-manager/release-23.05";
-    home-manager-l231028a.url = "github:nix-community/home-manager/release-23.05";
+    home-manager-l231028a.url = "github:nix-community/home-manager/release-23.11";
 
   };
 
