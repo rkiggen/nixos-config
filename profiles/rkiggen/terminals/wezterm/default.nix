cf97c64b2678d58d6d1fc8c95a26506a1c1e002c
Kiggen, Rob <rob.kiggen@gmail.com>
2025-04-04 14:54:12 +0200

    Change wezterm configuration

--- profiles/rkiggen/terminals/wezterm/default.nix
+++ profiles/rkiggen/terminals/wezterm/default.nix
@@ -1,21 +1,5 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-with lib; let
-  cfg = config.modules.terminals.wezterm;
-in {
-  options.modules.terminals.wezterm = {
-    enable = mkEnableOption "enable wezterm terminal emulator";
-  };
-
-  config = mkIf cfg.enable {
-    programs.wezterm = {
-      enable = true;
-      package = pkgs.wezterm-nightly;
-      extraConfig = builtins.readFile ./config.lua;
-    };
-  };
+{ nixpkgs, ...}: {
+  home.packages = [
+    nixpkgs.from.stable.wezterm
+  ];
 }
