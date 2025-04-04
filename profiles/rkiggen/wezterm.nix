926ddc2508814f9157de0a06e5a363c3f86b69b0
Kiggen, Rob <rob.kiggen@gmail.com>
2025-04-04 22:40:05 +0200

    Change: add wezterm configuration

--- profiles/rkiggen/wezterm.nix
+++ profiles/rkiggen/wezterm.nix
@@ -8,7 +8,8 @@
   	enable = true;
 	enableZshIntegration = true;
 	enableBashIntegration = true;
-	#extraConfig = builtins.readFile ./wezterm.lua  # use this line instead of the home.file."config" line when you want to change your wezterm config without rebuilding !
-  }
-  home.file.".config/wezterm/wezterm.lua".source = ./wezterm.lua;
+	extraConfig = builtins.readFile ./dotfiles/wezterm/wezterm.lua;  # use this line instead of the home.file."config" line when you want to change your wezterm config without rebuilding !
+  };
+
+  # home.file.".config/wezterm/wezterm.lua".source = ./wezterm.lua;
 }
