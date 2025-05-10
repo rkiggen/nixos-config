9af1eb2bb1d37a7d83f6f90b1389148d34c39493
Kiggen, Rob <rob.kiggen@gmail.com>
2025-05-10 14:32:21 +0200

    feature (fonts): add fonts module

--- hosts/l241214a/default.nix
+++ hosts/l241214a/default.nix
@@ -9,6 +9,7 @@
         ../../modules/xorg.nix
         ../../modules/graphical.nix
         ../../modules/console.nix
+        ../../modules/fonts.nix
         ../../modules/firefox.nix
         ../../modules/rofi.nix
         ../../modules/zsh.nix
---
+++ modules/fonts.nix
@@ -0,0 +1,50 @@
+# Fonts: https://nixos.wiki/wiki/Fonts
+
+{ inputs, pkgs, nixpkgs, ... }: {
+
+    config = {
+        environment.systemPackages = [
+            # Fonts
+            nixpkgs.from.stable.corefonts
+            nixpkgs.from.stable.liberation_ttf
+            nixpkgs.from.stable.noto-fonts
+            nixpkgs.from.stable.noto-fonts-cjk-sans
+            nixpkgs.from.stable.noto-fonts-emoji
+            nixpkgs.from.stable.fira
+            nixpkgs.from.stable.fira-code
+            nixpkgs.from.stable.fira-code-symbols
+            nixpkgs.from.stable.fira-mono
+            nixpkgs.from.stable.inconsolata
+            nixpkgs.from.stable.profont
+            nixpkgs.from.stable.source-sans-pro
+            nixpkgs.from.stable.source-serif-pro
+            nixpkgs.from.stable.symbola
+            nixpkgs.from.stable.ubuntu_font_family
+            nixpkgs.from.stable.unifont
+            nixpkgs.from.stable.vistafonts           
+        ];
+
+        fonts.packages = [
+            nixpkgs.from.stable.corefonts
+            nixpkgs.from.stable.liberation_ttf
+            nixpkgs.from.stable.noto-fonts
+            nixpkgs.from.stable.noto-fonts-cjk-sans
+            nixpkgs.from.stable.noto-fonts-emoji
+            nixpkgs.from.stable.fira
+            nixpkgs.from.stable.fira-code
+            nixpkgs.from.stable.fira-code-symbols
+            nixpkgs.from.stable.fira-mono
+            nixpkgs.from.stable.inconsolata
+            nixpkgs.from.stable.profont
+            nixpkgs.from.stable.source-sans-pro
+            nixpkgs.from.stable.source-serif-pro
+            nixpkgs.from.stable.symbola
+            nixpkgs.from.stable.ubuntu_font_family
+            nixpkgs.from.stable.unifont
+            nixpkgs.from.stable.vistafonts  
+        ];
+
+
+    };
+
+}
