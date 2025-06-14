772982a429dfea9ac6f52ed544c11d007af36461
Kiggen, Rob <rob.kiggen@gmail.com>
2025-06-14 10:31:45 +0200

    remove airscan and keep only brscan5

--- modules/scanning.nix
+++ modules/scanning.nix
@@ -11,7 +11,7 @@
         hardware.sane = {
             enable = true;
             extraBackends = [ 
-                nixpkgs.from.stable.sane-airscan
+                #nixpkgs.from.stable.sane-airscan
                 nixpkgs.from.stable.brscan5 
             ];
             brscan5 = {
@@ -21,33 +21,43 @@
                 };
             };
             openFirewall = true;
-            netConf = "10.48.6.66";
+            #netConf = "10.48.6.66";
         };
 
         # Allow discovery of Canon scanners 
         # Discovery of Canon scanners works via multicast. 
         # SANE sends BJNP discovery messages from all local addresses to ff02::1, UDP port 8612. 
         # Scanners receiving the message respond via unicast UDP.
-        networking.firewall = {
-            allowedUDPPorts = [ 8612 ];
-        };
+        #networking.firewall = {
+        #    allowedUDPPorts = [ 8610 8612 ]; 
+        #};
 
         services.udev.packages = [ 
-            nixpkgs.from.stable.sane-airscan 
             nixpkgs.from.stable.brscan5 
         ];
 
-        #environment.etc."sane.d/airscan.conf".text = ''
-        #    [devices]
-        #    Canon@Home = http://10.48.6.66:443/eSCL/, eSCL
+        services.udev.extraRules = ''
+            # Brother ADS-1700W - brscan5
+            ATTRS{idVendor}=="04f9", ATTRS{idProduct}=="60e0", MODE="0664", GROUP="scanner", ENV{libsane_matched}="yes"
+        '';
+
+        # https://wiki.debian.org/SaneOverNetwork#Canon_Multi-Function__Printers__and_CanonScan_Scanners_via_the_Network
+        #environment.etc."sane.d/pixma.conf".text = ''
+        #    bjnp://10.48.6.66 
         #'';
+
+        # add brother ads1700w 
+        environment.etc."sane.d/net.conf".text = ''
+            10.48.5.2
+        '';
 
         # Enable wireless scanning via airscan
         environment.systemPackages = [
             ## Backends
-            nixpkgs.from.stable.sane-airscan    # Scanner Access Now Easy - Apple AirScan (eSCL) driver
+            #nixpkgs.from.stable.sane-airscan    # Scanner Access Now Easy - Apple AirScan (eSCL) driver
+            #nixpkgs.from.stable.sane-backends   # SANE backends: includes sane-pixma for Canon i-Sensys MFC645c scanning
             nixpkgs.from.stable.brscan5         # Brother brscan5 sane backend driver
-            nixpkgs.from.stable.dsseries
+            #nixpkgs.from.stable.dsseries
 
             ## Frontends
             nixpkgs.from.stable.simple-scan
