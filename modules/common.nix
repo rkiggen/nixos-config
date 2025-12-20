e8e36a6a9d3aebc906255df42e420ef59ac3fa89
Kiggen, Rob <rob.kiggen@gmail.com>
2025-12-20 22:55:39 +0100

    changed default browser

--- modules/common.nix
+++ modules/common.nix
@@ -63,7 +63,7 @@
         # Set the default editor to neovim
         environment.variables = {
             EDITOR = "nvim";
-            BROWSER = "firefox-esr";
+            BROWSER = "firefox";
         };
 
 
