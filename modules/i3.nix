e4823c2af8e3d365b1ef955335b9d8566e72c3e7
Kiggen, Rob <rob.kiggen@gmail.com>
2025-12-21 11:03:55 +0100

    i3: add autorandr package

--- modules/i3.nix
+++ modules/i3.nix
@@ -18,6 +18,7 @@
         environment.systemPackages = [
             nixpkgs.from.stable.lxappearance    # lightweight program for configuring the theme and fonts of gtk applications
             nixpkgs.from.stable.dunst           # lightweight and customizable notification daemon
+            nixpkgs.from.stable.autorandr       # automatically select a display configuration based on connected devices
             nixpkgs.from.stable.feh             # light-weight image viewer
             nixpkgs.from.stable.lm_sensors      # tools for reading hardware sensors
             nixpkgs.from.stable.bc              # GNU software calculator
