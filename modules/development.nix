1759cb156863630839bea42e31a81164b023d27c
Kiggen, Rob <rob.kiggen@gmail.com>
2026-03-09 15:23:08 +0100

    development (change): added devpod cli

--- modules/development.nix
+++ modules/development.nix
@@ -10,6 +10,7 @@
             nixpkgs.from.stable.direnv                              # shell extension for managing your environment 
             nixpkgs.from.stable.claude-code                         # agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster
             nixpkgs.from.stable.devpod-desktop                      # Codespaces but open-source, client-only and unopinionated: Works with any IDE and lets you use any cloud, kubernetes or just localhost docker
+            nixpkgs.from.stable.devpod                              # Codespaces but open-source, client-only and unopinionated: Works with any IDE and lets you use any cloud, kubernetes or just localhost docker
         ];
     };
 }
