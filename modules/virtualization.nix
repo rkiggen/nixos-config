b22479abe2459de452f481a08530683b7d5c2816
Kiggen, Rob <rob.kiggen@gmail.com>
2026-03-09 15:33:23 +0100

    virtualization (change): enabled docker compatibility for podman

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -40,7 +40,7 @@
                 enable = true;
 
                 # Create a `docker` alias for podman, to use it as a drop-in replacement
-                dockerCompat = false;
+                dockerCompat = true;
 
                 # Required for containers under podman-compose to be able to talk to each other.
                 defaultNetwork.settings.dns_enabled = true;
