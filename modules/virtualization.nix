eb46bbba4a71195982ecfb0711e39714a2bf0cf2
Kiggen, Rob <rob.kiggen@gmail.com>
2026-05-23 20:42:30 +0200

    feature virtualization: add arm64 emulation

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -10,6 +10,14 @@
         # sure that the appropriate kernel modules are loaded
         # boot.kernelModules = [ "kvm-amd" "kvm-intel" ];
 
+        # Emulate arm64 & riscv64 on a native x86_64 (amd64) host 
+        boot.binfmt.emulatedSystems = [
+            "aarch64-linux"
+            "riscv64-linux"
+        ];
+        
+        boot.binfmt.registrations."aarch64-linux".fixBinary = true;
+
         virtualisation = {
             libvirtd = {
 
