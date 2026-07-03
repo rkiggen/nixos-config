92f1101e39cd0fb7aa2b9a374e52c8ea2f26ca02
Kiggen, Rob <rob.kiggen@gmail.com>
2026-07-03 08:45:04 +0200

    development - add jq

--- modules/development.nix
+++ modules/development.nix
@@ -6,6 +6,7 @@
             
             # development
             nixpkgs.for.rider.jetbrains.rider                       # jetbrains rider ide 
+            nixpkgs.from.stable.jq                                  # lightweight and flexible command-line JSON processor
             nixpkgs.from.stable.vscode                              # visual studio code (development ide)
             nixpkgs.from.stable.direnv                              # shell extension for managing your environment 
             nixpkgs.from.stable.claude-code                         # agentic coding tool that lives in your terminal, understands your codebase, and helps you code faster
